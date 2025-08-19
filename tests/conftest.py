961b62b220902d93a33f363732be7eea36d8c981
Giorgi Shakiashvili <giorgishakiashviili21@gmail.com>
2025-08-19 17:43:01 +0400

    It would gonna work

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,13 +2,27 @@ import pytest
 import json
 import os
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
 
 
-# from selenium.webdriver.chrome.options import Options
- #  Add this while running tests in locally
-    # options = Options()
-    # options.add_argument("--guest")
+@pytest.fixture(scope="session")
+def driver():
+    options = Options()
 
+    if os.getenv("CI"):  # Running in GitHub Actions
+        options.add_argument("--headless")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
+        options.add_argument("--disable-gpu")
+    else:  # Local run
+        options.add_argument("--guest")
+
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service, options=options)
+    yield driver
+    driver.quit()
 
 
 @pytest.fixture(scope="session")
