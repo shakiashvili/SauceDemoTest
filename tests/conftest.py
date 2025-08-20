bbea590eb9e5441f3620c974660b282ce9a06e45
Giorgi Shakiashvili <giorgishakiashviili21@gmail.com>
2025-08-20 11:01:27 +0400

    Done

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,20 +2,14 @@ import pytest
 import json
 import os
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
 
 
 @pytest.fixture(scope="session")
 def driver():
     options = Options()
 
-    
-    options.add_argument("--guest")
-
-    service = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service, options=options)
+    driver = webdriver.Chrome(options=options)
     yield driver
     driver.quit()
 
