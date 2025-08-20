6d0088526227ea7b4884e3a982e737151a8f0c1e
Giorgi Shakiashvili <giorgishakiashviili21@gmail.com>
2025-08-20 09:00:26 +0400

    Done

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,13 +11,8 @@ from webdriver_manager.chrome import ChromeDriverManager
 def driver():
     options = Options()
 
-    if os.getenv("CI"):  # Running in GitHub Actions
-        options.add_argument("--headless")
-        options.add_argument("--no-sandbox")
-        options.add_argument("--disable-dev-shm-usage")
-        options.add_argument("--disable-gpu")
-    else:  # Local run
-        options.add_argument("--guest")
+    
+    options.add_argument("--guest")
 
     service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service, options=options)
